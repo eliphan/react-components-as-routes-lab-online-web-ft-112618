7dcbd3cb4b33c44055534a770e896b5136fe71be
Elise Phan <aliceconnic@gmail.com>
2019-05-08 22:28:59 +0000

    Automatically backed up by Learn

--- src/containers/App.js
+++ src/containers/App.js
@@ -9,6 +9,7 @@ import Actors from '../components/Actors';
 import Directors from '../components/Directors';
 import Movies from '../components/Movies';
 
+<<<<<<< HEAD
 const App = (props) => {
   return (
      <Router>
@@ -21,6 +22,25 @@ const App = (props) => {
           <Route path="/actors" render={ Actors }/>
         </React.Fragment>
        </div>
+=======
+const NavBar = () => 
+  <div>
+    <NavLink to="/">Home</NavLink>
+    <NavLink to="/movies">Movies</NavLink>
+    <NavLink to="/directors">Directors</NavLink>
+    <NavLink to="/actors">Actors</NavLink>
+  </div>
+
+const App = (props) => {
+  return (
+     <Router>
+       <React.Fragment>
+         <Route path="/" render={ Home }/>
+         <Route path="/movies" render={ Movies }/>
+         <Route path="/directors" render={ Directors }/>
+         <Route path="/actors" render={ Actor }/>
+       </React.Fragment>
+>>>>>>> a4eb8fe9061db3c37ced323407f7aabde1bd76a9
      </Router>
   );
 };
